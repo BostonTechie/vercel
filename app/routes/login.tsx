bfb8c092eba4d8b4798499b6b66c02311136961e
Andrew D Urquhart <andrewurq@gmail.com>
2022-08-12 09:48:53 -0400

    :tada: added defi #2450A0 as a theme color

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -4,6 +4,52 @@
 import { Layout } from "~/components/layout"
 import { FormField } from "~/components/form-field"
 import { useState } from 'react'
+import { ActionFunction, json } from "@remix-run/node"
+import { validateEmail, validatePassword } from '../utlis/validators.server'
+import { login, register } from '~/utlis/auth.server'
+
+// @47 min
+export const action: ActionFunction = async ({ request }) => {
+    const form = await request.formData()
+    const action = form.get('_action')
+    const email = form.get('email')
+    const password = form.get('password')
+
+    if (
+        typeof action !== 'string' ||
+        typeof email !== 'string' ||
+        typeof password !== 'string') {
+        return json({ error: `Invalid Form Data`, form: action }, { status: 400 })
+    }
+
+
+    // ignored some steps at 52 min
+    const errors = {
+        email: validateEmail(email),
+        password: validatePassword(password),
+
+    }
+
+    if (Object.values(errors).some(Boolean))
+        return json({ errors, fields: { email, password }, form: action }, { status: 400 })
+
+    // validation ...
+
+    switch (action) {
+        case 'login': {
+            return await login({ email, password })
+        }
+        case 'register': {
+
+            return await register({ email, password })
+        }
+        default:
+            return json({ error: `Invalid Form Data` }, { status: 400 });
+    }
+
+
+}
+
 
 export default function Login() {
 
@@ -37,8 +83,8 @@ export default function Login() {
 
             <div className="h-full flex justify-center items-center flex-col gap-y-4">
 
-                <div className="rounded-2xl bg-gray-200 p-6 w-60 gap-y-4">
-                    <form>
+                <div >
+                    <form className="rounded-2xl bg-gray-300 p-6 w-60 gap-y-4" method="post">
                         <FormField
                             htmlFor="email"
                             label="Email"
@@ -55,13 +101,15 @@ export default function Login() {
                             value={formData.password}
                             onChange={e => handleInputChange(e, 'password')}
                         />
+                        <br></br>
                         {action === 'login' ? (
+
 
                             <button
                                 type="submit"
                                 name="_action"
                                 value={action}
-                                className="w-full rounded bg-blue-500 py-2 px- text-white hover:bg-blue-700 focus:bg-blue-400"
+                                className="w-full rounded bg-defi py-2 px- text-white hover:bg-blue-400 focus:bg-blue-400"
                             >
                                 Log in
                             </button>
@@ -70,17 +118,19 @@ export default function Login() {
                                 type="submit"
                                 name="_action"
                                 value={action}
-                                className="w-full rounded bg-blue-500 py-2 px- text-white hover:bg-blue-700 focus:bg-blue-400"
+                                className="w-full rounded bg-defi py-2 px- text-white hover:bg-blue-400 focus:bg-blue-400"
                             >
-                                Sign in
+                                Sign up
                             </button>)}
 
                     </form>
 
                 </div>
+
+                {/* this button toggles between letting user sign up and logging in */}
                 <button
                     onClick={() => setAction(action == 'login' ? 'sign-up' : 'login')}
-                    type="submit" className="rounded bg-blue-500 py-2 px- text-white hover:bg-blue-700 focus:bg-blue-400" >Already have an account?
+                    type="submit" className="rounded bg-defi py-2 px- text-white hover:bg-blue-700 focus:bg-blue-400" >Sign me up!
                 </button>
             </div>
         </Layout>
--- app/utlis/auth.server.ts
+++ app/utlis/auth.server.ts
@@ -1,12 +1,33 @@
 // app/utils/auth.server.ts
 // https://www.youtube.com/watch?v=vR33ZRJekHk @ 39:35
 
-import { json } from '@remix-run/node'
+import { json, createCookieSessionStorage, redirect } from '@remix-run/node'
 import type { RegisterForm, LoginForm } from './types.server'
 import { prisma } from './prisma.server'
 import { createUser } from './users.server'
+import bcrypt from 'bcryptjs'
 
 
+//throw an error if you don't set up a session secret within .env @ 42 min
+const sessionSecret = process.env.SESSION_SECRET
+if (!sessionSecret) {
+  throw new Error('SESSION_SECRET must be set')
+}
+
+// set up the cookie storage 
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: 'splinter-session',
+    secure: process.env.NODE_ENV === 'production',
+    secrets: [sessionSecret],
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+})
+
+//register a new user here redirect to /hive the table that shows data
 export async function register(user: RegisterForm) {
   const exists = await prisma.user.count({ where: { email: user.email } })
   if (exists) {
@@ -25,9 +46,10 @@ export async function register(user: RegisterForm) {
     )
   }
 
-  return null
+  return createUserSession(newUser.id, '/hive')
 }
 
+//login a pre existing user here redirect successful login to /hive
 export const login = async(form: LoginForm) => {
     const user = await prisma.user.findUnique({
         where: {email: form.email},
@@ -36,4 +58,17 @@ export const login = async(form: LoginForm) => {
     if(!user || !(await bcrypt.compare(form.password, user.password))){
         return json({error: "incorrect loging"}, {status: 400})
     }
+
+    return createUserSession(user.id, "/hive")
+}
+
+// create the user session and redirect to another page
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession()
+  session.set('userId', userId)
+  return redirect(redirectTo, {
+    headers: {
+      'Set-Cookie': await storage.commitSession(session),
+    },
+  })
 }
