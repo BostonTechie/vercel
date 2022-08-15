a515c6f9e6291e8bd8735737a4c48b8aae5695fb
Andrew D Urquhart <andrewurq@gmail.com>
2022-08-15 16:48:47 -0400

    css

--- app/routes/hive.tsx
+++ app/routes/hive.tsx
@@ -20,6 +20,7 @@ import {
 
 } from '@heroicons/react/outline'
 
+
 //@1hr 9min https://www.youtube.com/watch?v=vR33ZRJekHk
 export const loader: LoaderFunction = async ({ request }) => {
     await requireUserId(request)
@@ -229,7 +230,7 @@ export default function Hive() {
                                                 type="button"
                                                 className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
                                             >
-                                                Add user
+                                                Export
                                             </button>
                                         </div>
                                     </div>
@@ -239,71 +240,71 @@ export default function Hive() {
                                             <div className="inline-block min-w-full py-2 align-middle">
                                                 <div className="shadow-sm ring-1 ring-black ring-opacity-5">
                                                     <table className="min-w-full border-separate" style={{ borderSpacing: 0 }}>
-                                                        <thead className="bg-gray-50">
+                                                        <thead className="bg-gray-5000">
                                                             <tr>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-500 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
                                                                 >
                                                                     dbid#
                                                                 </th>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
+                                                                    className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-500 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
                                                                 >
                                                                     Asset
                                                                 </th>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
+                                                                    className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-500 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
                                                                 >
                                                                     From
                                                                 </th>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-500 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                                                                 >
                                                                     To
                                                                 </th>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-500 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                                                                 >
                                                                     Quant
                                                                 </th>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-500 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                                                                 >
                                                                     Price
                                                                 </th>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-500 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                                                                 >
                                                                     Gross
                                                                 </th>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-500 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                                                                 >
                                                                     Net
                                                                 </th>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-500 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                                                                 >
                                                                     Trans
                                                                 </th>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-500 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                                                                 >
                                                                     Debit
                                                                 </th>
                                                                 <th
                                                                     scope="col"
-                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                                                                    className="sticky top-0 z-10 border-b border-gray-300 bg-gray-500 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                                                                 >
                                                                     Credit
                                                                 </th>
@@ -359,7 +360,14 @@ export default function Hive() {
                                                                         {data.Quantity}
                                                                     </td>
 
-                                                                    <td>{data.Token_Price}</td>
+                                                                    <td
+                                                                        className={classNames(
+                                                                            dataIdx !== cryptoData.length - 1 ? 'border-b border-gray-200' : '',
+                                                                            'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8'
+                                                                        )}
+                                                                    >
+                                                                        {data.Token_Price}
+                                                                    </td>
 
                                                                     <td
                                                                         className={classNames(
