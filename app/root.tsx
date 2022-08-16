d776107f5cd2223d3a32d1cc381cea5c4a8a219c
Andrew D Urquhart <andrewurq@gmail.com>
2022-08-16 11:09:22 -0400

    remove excel scripts

--- app/root.tsx
+++ app/root.tsx
@@ -26,8 +26,6 @@ export default function App() {
       <head>
         <Meta />
         <Links />
-        <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" ></script>
-
       </head>
       <body>
         <Outlet />
