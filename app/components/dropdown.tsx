124c0ad25f3a75b07242fa31cace1704ec73a08b
Andrew D Urquhart <andrewurq@gmail.com>
2022-08-17 16:31:53 -0400

    :tada" query page setting up limit

--- app/components/dropdown.tsx
+++ app/components/dropdown.tsx
@@ -3,17 +3,17 @@ import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 
-const people = [
-    { id: 1, name: 'Wade Cooper' },
-    { id: 2, name: 'Arlene Mccoy' },
-    { id: 3, name: 'Devon Webb' },
-    { id: 4, name: 'Tom Cook' },
-    { id: 5, name: 'Tanya Fox' },
-    { id: 6, name: 'Hellen Schmidt' },
-    { id: 7, name: 'Caroline Schultz' },
-    { id: 8, name: 'Mason Heaney' },
-    { id: 9, name: 'Claudie Smitham' },
-    { id: 10, name: 'Emil Schaefer' },
+const queryReturn = [
+    { id: 1, number: 1 },
+    { id: 2, number: 10 },
+    { id: 3, number: 100 },
+    { id: 4, number: 1000 },
+    { id: 5, number: 10000 },
+    { id: 6, number: 100000 },
+    { id: 7, number: 250000 },
+    { id: 8, number: 500000 },
+    { id: 9, number: 750000 },
+    { id: 10, number: 1000000 },
 ]
 
 function classNames(...classes) {
@@ -21,16 +21,16 @@ function classNames(...classes) {
 }
 
 export function Dropdown() {
-    const [selected, setSelected] = useState(people[3])
+    const [selected, setSelected] = useState(queryReturn[2])
 
     return (
         <Listbox value={selected} onChange={setSelected}>
             {({ open }) => (
                 <>
-                    <Listbox.Label className="block text-sm font-medium text-gray-700">Assigned to {JSON.stringify(selected)}</Listbox.Label>
+                    <Listbox.Label className="block text-sm font-medium text-gray-700">Limit: {selected.number} </Listbox.Label>
                     <div className="mt-1 relative">
                         <Listbox.Button className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                            <span className="block truncate">{selected.name}</span>
+                            <span className="block truncate">{selected.number}</span>
                             <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                                 <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
                             </span>
@@ -44,21 +44,21 @@ export function Dropdown() {
                             leaveTo="opacity-0"
                         >
                             <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                                {people.map((person) => (
+                                {queryReturn.map((limit) => (
                                     <Listbox.Option
-                                        key={person.id}
+                                        key={limit.id}
                                         className={({ active }) =>
                                             classNames(
                                                 active ? 'text-white bg-indigo-600' : 'text-gray-900',
                                                 'cursor-default select-none relative py-2 pl-3 pr-9'
                                             )
                                         }
-                                        value={person}
+                                        value={limit}
                                     >
                                         {({ selected, active }) => (
                                             <>
                                                 <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                                                    {person.name}
+                                                    {limit.number}
                                                 </span>
 
                                                 {selected ? (
