1048d467d5f2b3a711ed8358712da8c59194e363
Andrew D Urquhart <andrewurq@gmail.com>
2022-08-18 15:03:14 -0400

    :tada: added an iframe example

--- app/routes/iframe.tsx
+++ app/routes/iframe.tsx
@@ -15,7 +15,7 @@ export const loader: LoaderFunction = async ({ request }) => {
     return null
 }
 
-export default function Iframe() {
+export default function Home() {
     return (<Layout>
         <Sidebar />
         <div className="mt-8 flex flex-col float-right w-9/12">
@@ -24,10 +24,15 @@ export default function Iframe() {
 
                     {/* put your data into the main section below */}
 
-                    <main ><h2>look at me I am an i frame</h2>
+                    <main ><h2>hi i am home page, I am under construction</h2>
 
                         <p><Link to="../hive" style={{ color: "blue" }}>Click here for the hive data</Link></p><p>or hit the reports icon to the left to make a new query</p></main>
-
+                    <iframe id="inlineFrameExample"
+                        title="Inline Frame Example"
+                        width="300"
+                        height="200"
+                        src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+                    </iframe>
 
                 </div>
             </div>
